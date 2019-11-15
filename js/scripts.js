ad292a5cca867d9f94f7346d230ca0f16b64c5ac
Amir Ashvins <ali.washangton.98@gmail.com>
2019-11-14 16:50:49 -0800

    Added the calendar

--- js/scripts.js
+++ js/scripts.js
@@ -21,8 +21,8 @@ function loadDBToStorage() {
         querySnapshot.forEach(function (doc) {
             assignmentDetails = doc.data();
             newAssignment = new assignment(assignmentDetails['course'], assignmentDetails['name'], assignmentDetails['dueDate'],
-            assignmentDetails['dueTime'], assignmentDetails['d2lLink'], assignmentDetails['instructions'], 
-            assignmentDetails['additionalInformation'], assignmentDetails['instructorID']);
+                assignmentDetails['dueTime'], assignmentDetails['d2lLink'], assignmentDetails['instructions'],
+                assignmentDetails['additionalInformation'], assignmentDetails['instructorID']);
             assignmentList.push(newAssignment);
             buildListRow(newAssignment);
         });
@@ -105,5 +105,64 @@ function getUrlQueries() {
     return queries;
 }
 
+let today = new Date();
+let currentMonth = today.getMonth();
+let currentYear = today.getFullYear();
 
+let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+let monthAndYear = document.getElementById('monthAndYear')
+
+showCalendar(currentMonth, currentYear);
+
+
+function showCalendar(month, year) {
+    let firstDay = new Date(year, month).getDate();
+    let daysInMonth = 32 - new Date(year, month, 32).getDate();
+
+    let tbl = document.getElementById('calendar-body');
+
+    tbl.innerHTML = '';
+    monthAndYear.innerHTML = months[month] + " " + year;
+
+    let date = 1;
+
+    for(let i = 0; i < 6 ; i++) {
+        let row = document.createElement('tr');
+
+        for (let j = 0; j < 7; j++){
+            
+            if (i === 0 && j < firstDay) {
+                console.log(firstDay);
+                let cell = document.createElement('td');
+                let cellText = document.createTextNode("");
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }else if (date > daysInMonth) {
+                break;
+            }else {
+                let cell = document.createElement('td');
+                let cellText = document.createElement(date);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+
+            date ++;
+        }
+        tbl.appendChild(row);
+    }
+}
+
+function previous() {
+    currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
+    currentMonth = currentMonth === 0 ? 11 : currentMonth - 1;
+    showCalendar(currentMonth, currentYear);
+
+}
+function next () {
+    currentYear = currentMonth === 11 ? currentYear + 1 : currentYear;
+    currentMonth = (currentMonth + 1) % 12;
+    showCalendar(currentMonth, currentYear);
+
+}
 
